fbd8727216b9b9db8dc100bd4ce9e563531b825d
Yuet Hang Chau <yhc40@cam.ac.uk>
2021-11-13 14:35:19 +0000

    lol

--- UI.py
+++ UI.py
@@ -5,15 +5,18 @@ from PIL import ImageTk, Image
 import random
 import os
 root = Tk()
-root.geometry("405x640")
+root.geometry("380x640")
 root.resizable(0, 0)
 
-img = ImageTk.PhotoImage(Image.open("walkingtrim.png").resize((300,205), Image.ANTIALIAS))
+img = ImageTk.PhotoImage(Image.open("walking_cathay.png").resize((300,205), Image.ANTIALIAS))
 panel = Label(root, image = img)
-panel.place(x=150,y=0)
-asian = ImageTk.PhotoImage(Image.open("asiamiles.png").resize((40,40), Image.ANTIALIAS))
+panel.place(x=50,y=0)
+asian = ImageTk.PhotoImage(Image.open("asiamiles.png").resize((28,28), Image.ANTIALIAS))
 a = Label(root,image=asian)
 a.place(x= 50, y=300)
+asianmiles = Label(root,text="2")
+asianmiles.config(font=("Arial", 30))
+asianmiles.place(x=100,y=293)
 c = 0
 def walk(label):
     c = 0
@@ -24,7 +27,7 @@ def walk(label):
         label.after(random.randint(100,700),count)
     count()
 rc = tk.Label(root,fg="dark green")
-rc.place(x=30,y=400)
+rc.place(x=100,y=505)
 walk(rc)
 
 tk.mainloop()
