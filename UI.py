1915dd3ed11ceb74867c47ea7a4a4f3ad1b833e0
Yuet Hang Chau <yhc40@cam.ac.uk>
2021-11-13 10:25:51 +0000

    try basic

--- UI.py
+++ UI.py
@@ -1,9 +1,25 @@
 from tkinter import *
 from tkinter import ttk
+import tkinter as tk
+from PIL import ImageTk, Image
+import os
+
 root = Tk()
 root.geometry("800x800")
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+img = ImageTk.PhotoImage(Image.open("walking.png"))
+panel = Label(root, image = img)
+panel.pack(side = "bottom", fill = "both", expand = "yes")
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(frame, 
+                   text="QUIT", 
+                   fg="red",
+                   command=quit)
+button.pack(side=tk.LEFT)
+slogan = tk.Button(frame,
+                   text="Hello")
+slogan.pack(side=tk.LEFT)
+
 root.mainloop()
