2359431b8b841a38982779b399b56c61b97f89c6
SChan11E6 <94214942+SChan11E6@users.noreply.github.com>
2021-11-14 06:05:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,42 +10,42 @@ import voteUI
 def cancel():
     window.destroy()
 
-window=Tk()
-window.title("Portal")
-window.geometry("800x460")
-window.resizable(0,0)
+root=Tk()
+root.title("Portal")
+root.geometry("800x460")
+root.resizable(0,0)
 
-l1=Label(window, text="Healthy Lifestyle")
-l1.grid(sticky="W", row=0, column=0)
+l1=Label(root, text="Healthy Lifestyle")
+l1.grid(row=0, column=0)
 
-l1=Label(window, text="Gacha")
-l1.grid(sticky="W", row=0, column=1)
+l1=Label(root, text="Gacha")
+l1.grid(row=0, column=1)
 
-l1=Label(window, text="Random Box")
-l1.grid(sticky="W", row=0, column=2)
+l1=Label(root, text="Random Box")
+l1.grid(row=0, column=2)
 
-l1=Label(window, text="Community")
-l1.grid(sticky="W", row=0, column=3)
+l1=Label(root, text="Community")
+l1.grid(row=0, column=3)
 
-b1=Button(window, text="Go", width=12, command=UI.run)
-b1.grid(sticky="E", row=1, column=0)
+b1=Button(root, text="Go", width=12, command=UI.run)
+b1.grid(row=1, column=0)
 
-b1=Button(window, text="Go", width=12, command=gacha.gachaa)
-b1.grid(sticky="E", row=1, column=1)
+b1=Button(root, text="Go", width=12, command=gacha.gachaa)
+b1.grid(row=1, column=1)
 
-b1=Button(window, text="Go", width=12, command=randomitem.random_item)
-b1.grid(sticky="E", row=1, column=2)
+b1=Button(root, text="Go", width=12, command=randomitem.random_item)
+b1.grid(row=1, column=2)
 
-b1=Button(window, text="Go", width=12, command=community)
-b1.grid(sticky="E", row=1, column=3)
+b1=Button(root, text="Go", width=12, command=community)
+b1.grid(row=1, column=3)
 
-b1=Button(window, text="Close Window", width=12, command=cancel)
-b1.grid(sticky="W", row=2, column=3)
+b1=Button(root, text="Close Window", width=12, command=cancel)
+b1.grid(sticky="E", row=2, column=3)
 
-col_count, row_count = window.grid_size()
+col_count, row_count = root.grid_size()
 
 for col in range(col_count):
-	window.grid_columnconfigure(col, minsize=190)
+	root.grid_columnconfigure(col, minsize=190)
 
 for row in range(row_count):
-	window.grid_rowconfigure(row, minsize=50)
+	root.grid_rowconfigure(row, minsize=50)
