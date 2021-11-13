b49ec2cd893cff408744facb61239db3a105a679
wallythek <78818103+wallythek@users.noreply.github.com>
2021-11-14 01:37:09 +0800

    Update userVote.py

--- userVote.py
+++ userVote.py
@@ -1,5 +1,6 @@
 import random
 import string
+from PIL import Image
 
 # modelling a user post, containing an image, a description, a vote count and the list of voters
 class UserPost:
@@ -39,9 +40,10 @@ class UserPost:
 # modelling a user, containing the id, the username, the profile description, the password, the number of miles
 # and the post (if any) the user posted
 class User:
-    def __init__(self, accId, username, descr, password):
+    def __init__(self, accId, username, profilePic, descr, password):
         self.__accId = accId
         self.__username = username
+        self.__profilePic = profilePic
         self.__descr = descr
         self.__password = password
         self.__miles = 0
@@ -52,7 +54,13 @@ class User:
     
     def setUsername(self, username):
         self.__username = username
+    
+    def getProfilePic(self):
+        return self.__profilePic
 
+    def changeProfilePic(self, profilePic):
+        self.__profilePic = profilePic
+    
     def getDescription(self):
         return self.__descr
 
@@ -105,12 +113,16 @@ class User:
 # assume 5 users in the prototype, and the current user is users[0]
 users = []
 for i in range(5):
-    username = "".join(random.choices(string.ascii_lowercase + string.digits, k = random.randint(6, 10))) + str(i)
+    username = "".join(random.choices(string.ascii_lowercase + string.digits,
+                k = random.randint(6, 10))) + str(i)
+    profilePic = random.choice([Image.open("images.jpg"), Image.open("images-1.jpg"),
+                Image.open("images-2.jpg"), Image.open("images-3.jpg"), Image.open("images-4.jpg")])
     descr = random.choice(["Hi", "Nice to meet you", "I'm fine thank you"])
-    password = "".join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits, k = random.randint(6, 10)))
-    
-    # ensures no duplicate usernames
-    while (username in [user.getUsername() for user in users]):
-        username = "".join(random.choices(string.ascii_lowercase + string.digits, k = random.randint(6, 10))) + str(i)
+    password = "".join(random.choices(string.ascii_lowercase
+                + string.ascii_uppercase +string.digits, k = random.randint(6, 10)))
 
-    users.append(User(i, username, descr, password))
+    while (username in [user.getUsername() for user in users]):
+        username = "".join(random.choices(string.ascii_lowercase + string.digits,
+                    k = random.randint(6, 10))) + str(i)
+
+    users.append(User(i, username, profilePic, descr, password))
