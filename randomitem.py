fea40e9a13b43fd0677567eefd3c126fa9e2b065
SChan11E6 <94214942+SChan11E6@users.noreply.github.com>
2021-11-14 05:47:22 +0800

    Create randomitem.py

---
+++ randomitem.py
@@ -0,0 +1,84 @@
+def random_item():
+
+	randdict = {"Phone":["iPhone13", "Samsung Z Flip 3", "Nokia 3310"], "Stationery":["Pen", "Eraser", "Calculator"], "Watch":["G-Shock", "Rolex", "Seiko"], "Experience":["3-days staycation in HK", "Round Trip to Japan", "Buffet voucher"]}
+	
+	def proceed():
+		window.destroy()
+		chooserand()
+
+	def cancel():
+		window.destroy()
+
+	def chooserand():
+		def choose():
+			    x = e1.get()
+			    y = random.choice(randdict[x])
+			    window.destroy()
+			    master = tk.Tk()
+			    master.withdraw()
+			    messagebox.showinfo('Prize', f"You got {y}!")
+
+
+		window=tk.Tk()
+		window.title("Rand surprise")
+		window.geometry("500x300")
+		window.resizable(0,0)
+		image1 = Image.open("randombox.jpg")
+		image1 = image1.resize((300, 300), Image.ANTIALIAS) ## The (250, 250) is (height, width)
+
+		test = ImageTk.PhotoImage(image1)
+
+		label1 = tk.Label(image=test)
+		label1.image = test
+
+	# Position image
+		label1.place(x=100, y=0)
+		x = 0
+		clicked=tk.StringVar()
+		e1=tk.ttk.Combobox(window, width=12, textvariable=clicked)
+		e1['values']=list(randdict.keys())
+		e1.grid(row=5, column=1)
+		e1.current(0)
+		e1.bind("<<ComboboxSelected>>",x)
+
+		b1=tk.Button(window, text="Draw", width=12, command=choose)
+		b1.grid(sticky="W", row=5, column=2)
+
+		b1=tk.Button(window, text="Cancel", width=12, command=cancel)
+		b1.grid(row=5, column=0)
+
+		col_count, row_count = window.grid_size()
+		for col in range(col_count):
+		    window.grid_columnconfigure(col, minsize=190)
+
+		for row in range(row_count):
+		    window.grid_rowconfigure(row, minsize=50)
+    
+		window.mainloop()
+
+	window=tk.Tk()
+	window.title("Random surprise")
+	window.geometry("740x100")
+	window.resizable(0,0)
+	
+	
+	l1=Label(window, text="Asia Miles you now have:")
+	l1.grid(sticky="W", row=0, column=1)
+	
+	l1=Label(window, text=", are you sure you want to draw?")
+	l1.grid(sticky="W", row=0, column=2)
+	
+	b1=tk.Button(window, text="Yes!", width=12, command=proceed)
+	b1.grid(sticky="W", row=1, column=3)
+
+	b1=tk.Button(window, text="Not really", width=12, command=cancel)
+	b1.grid(sticky="E", row=1, column=0)
+
+	col_count, row_count = window.grid_size()
+
+	for col in range(col_count):
+		window.grid_columnconfigure(col, minsize=190)
+
+	for row in range(row_count):
+		window.grid_rowconfigure(row, minsize=50)
+	window.mainloop()
