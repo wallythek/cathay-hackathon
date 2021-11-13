fa186da7fa7737daf9a45d02f0259078f0f2fdb6
wallythek <78818103+wallythek@users.noreply.github.com>
2021-11-14 02:55:37 +0800

    Update voteUI.py

--- voteUI.py
+++ voteUI.py
@@ -13,8 +13,7 @@ users = []
 for i in range(5):
     username = "".join(random.choices(string.ascii_lowercase + string.digits,
                 k = random.randint(6, 10))) + str(i)
-    profilePic = random.choice([Image.open("images-0.jpg"), Image.open("images-1.jpg"),
-                Image.open("images-2.jpg"), Image.open("images-3.jpg"), Image.open("images-4.jpg")])
+    profilePic = Image.open("images-" + str(i) + ".jpg")
     descr = random.choice(["Hi", "Nice to meet you", "I'm fine thank you"])
     password = "".join(random.choices(string.ascii_lowercase
                 + string.ascii_uppercase +string.digits, k = random.randint(6, 10)))
@@ -27,6 +26,30 @@ for i in range(5):
 
 root = Tk()
 root.geometry("380x640")
-
 root.resizable(0, 0)
+
+username0 = Label(root,text=users[0].getUsername(),bg="white")
+username0.config(font=("Arial",20))
+username0.place(x=55,y=18)
+
+profile0 = ImageTk.PhotoImage(Image.open("images-0.jpg").resize((20,20), Image.ANTIALIAS))
+p0 = Label(root,image=profile0)
+p0.place(x=20,y=20)
+
+post0 = ImageTk.PhotoImage(Image.open("user_post.png").resize((185,185), Image.ANTIALIAS))
+p0 = Label(root,image=post0)
+p0.place(x=0,y=50)
+
+username1 = Label(root,text=users[1].getUsername(),bg="white")
+username1.config(font=("Arial",20))
+username1.place(x=240,y=18)
+
+profile1 = ImageTk.PhotoImage(Image.open("images-1.jpg").resize((20,20), Image.ANTIALIAS))
+p1 = Label(root,image=profile1)
+p1.place(x=205,y=20)
+
+post1 = ImageTk.PhotoImage(Image.open("user_post.png").resize((185,185), Image.ANTIALIAS))
+p1 = Label(root,image=post1)
+p1.place(x=185,y=50)
+
 tk.mainloop()
