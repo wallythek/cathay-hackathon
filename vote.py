f04055d70e581c28645a9562a9000729755ea8fa
Yuet Hang Chau <yhc40@cam.ac.uk>
2021-11-13 17:35:18 +0000

    lol

---
+++ vote.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+from PIL import ImageTk, Image
+import random
+import os
+root = Tk()
+root.geometry("380x640")
+
+root.resizable(0, 0)
+tk.mainloop()
