28e6940278ca8a5aa17c4b4a2d5d562a95b5818d
wallythek <78818103+wallythek@users.noreply.github.com>
2021-11-18 03:51:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,91 @@
 import os
+from userClass import User # classes to get the usernames of the users
+from userClass import UserPost
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
+import random
+import string
+from PIL import ImageTk, Image
 import UI
 import gacha
 import randomitem
 import voteUI
 
-def cancel():
-	root.destroy()
-def healthy():
-	cancel()
-	UI.run()
-def gaacha():
-	cancel()
-	gacha.gachaa()
-def randi():
-	cancel()
-	randomitem.random_item()
-def community():
-	cancel()
-	voteUI.vote()
-	
-root=Tk()
-root.title("Portal")
-root.geometry("800x160")
+users = [User(0, "me", Image.open("images-0.jpg"), "Me", "69420")]
+for i in range(1, 6):
+    username = "".join(random.choices(string.ascii_lowercase + string.digits,
+            k = random.randint(6, 10)))
+    profilePic = Image.open("images-" + str(i) + ".jpg")
+    descr = random.choice(["Hi", "Nice to meet you", "I'm fine thank you"])
+    password = "".join(random.choices(string.ascii_lowercase
+            + string.ascii_uppercase +string.digits, k = random.randint(6, 10)))
 
-l1=Label(root, text="Healthy Lifestyle")
-l1.grid(row=0, column=0)
+    while (username in [user.getUsername() for user in users]):
+        username = "".join(random.choices(string.ascii_lowercase + string.digits,
+            k = random.randint(6, 10)))
 
-l1=Label(root, text="Gacha")
-l1.grid(row=0, column=1)
+    users.append(User(i, username, profilePic, descr, password))
 
-l1=Label(root, text="Random Box")
-l1.grid(row=0, column=2)
+for i, user in enumerate(users):
+    post = UserPost(Image.open("user_post_"+str(i)+".png"), "hi")
+    user.addPost(post)
 
-l1=Label(root, text="Community")
-l1.grid(row=0, column=3)
+for _ in range(3):
+    for _ in range(3):
+        users[random.randint(0, 4)].votePost(users[random.randint(0, 4)].getPost())
+        users[random.randint(0, 4)].unvotePost(users[random.randint(0, 4)].getPost())
 
-b1=Button(root, text="Go", width=12, command=healthy)
-b1.grid(row=1, column=0)
+def main(users):
+    def cancel():
+            root.destroy()
+    def healthy():
+            cancel()
+            UI.run(users)
+            main(users)
+    def gaacha():
+            cancel()
+            gacha.gachaa()
+            main(users)
+    def randi():
+            cancel()
+            randomitem.random_item()
+            main(users)
+    def community(users_ = users):
+            cancel()
+            voteUI.vote(users_)
+            users_.sort(key = lambda x: x.getPost().getVoteCount(), reverse = True)
+            main(users)
+            
+    root=Tk()
+    root.title("Portal")
+    root.geometry("380x640")
+    root.configure(bg="#c6e5dc")
 
-b1=Button(root, text="Go", width=12, command=gaacha)
-b1.grid(row=1, column=1)
+    
+    
+    b1=Button(root, text="Healthy Lifestyle", width=15, height=2, command=healthy, bg="#046464")
+    b1.grid(row=2, column=1)
 
-b1=Button(root, text="Go", width=12, command=randi)
-b1.grid(row=1, column=2)
+    b1=Button(root, text="Gacha", width=15, height=2, command=gaacha, bg="#046464")
+    b1.grid(row=3, column=1)
 
-b1=Button(root, text="Go", width=12, command=community)
-b1.grid(row=1, column=3)
+    b1=Button(root, text="Random Box", width=15, height=2, command=randi, bg="#046464")
+    b1.grid(row=4, column=1)
 
-b1=Button(root, text="Close Window", width=12, command=cancel)
-b1.grid(sticky="E", row=2, column=3)
+    b1=Button(root, text="Community", width=15, height=2, command=community, bg="#046464")
+    b1.grid(row=5, column=1)
 
-col_count, row_count = root.grid_size()
+    b1=Button(root, text="Close Window", width=10, command=cancel, bg="#046464")
+    b1.grid(sticky="E", row=6, column=2)
 
-for col in range(col_count):
-	root.grid_columnconfigure(col, minsize=190)
+    col_count, row_count = root.grid_size()
 
-for row in range(row_count):
-	root.grid_rowconfigure(row, minsize=50)
-root.mainloop()
+    for col in range(col_count):
+            root.grid_columnconfigure(col, minsize=115)
+
+    for row in range(row_count):
+            root.grid_rowconfigure(row, minsize=90)
+    root.mainloop()
+
+main(users)
