ce9720b3f7fd56b97b89df215d8ac270d94d91fa
wallythek <78818103+wallythek@users.noreply.github.com>
2021-11-19 01:31:19 +0800

    Update voteUI.py

--- voteUI.py
+++ voteUI.py
@@ -21,6 +21,7 @@ def vote(users):
     root = Tk()
     root.title("Community")
     root.geometry("380x640")
+    root.config(bg="#c6e5dc")
     root.resizable(0, 0)
 
     usernameLabels = []
@@ -36,48 +37,48 @@ def vote(users):
     clist = []
     likeButtons = []
     dislikeButtons = []
-    tempimage = ImageTk.PhotoImage(Image.open("rounded_widget.png").resize((173,220), Image.ANTIALIAS))
+    tempimage = ImageTk.PhotoImage(Image.open("rounded_widget.png").resize((173,230), Image.ANTIALIAS))
     globallike = ImageTk.PhotoImage(Image.open("like.png").resize((20,20), Image.ANTIALIAS))
     globaldislike = ImageTk.PhotoImage(Image.open("dislike.png").resize((20,20), Image.ANTIALIAS))
     gshare= ImageTk.PhotoImage(Image.open("share.png").resize((20,20), Image.ANTIALIAS))
     comment = ImageTk.PhotoImage(Image.open("comment.png").resize((20,20), Image.ANTIALIAS))
 
-    homeButton = Button(root, text="Return to Home Page", command=root.destroy)
-    homeButton.place(x=200,y=10)
+    homeButton = Button(root, text="Return to Home Page", width=20, command=root.destroy)
+    homeButton.place(x=190,y=10)
     
     for i in range(len(users) // 2):
         for j in range(2):
          
-            roundies.append(Label(root,image=tempimage))
-            roundies[-1].place(x=20+185*j-gx, y=40+235*i)
+            roundies.append(Label(root,image=tempimage,bg="#c6e5dc"))
+            roundies[-1].place(x=17+185*j-gx, y=38+235*i)
 
-            likey.append(Label(root,image=globallike,bg="#c6e5dc"))
+            likey.append(Label(root,image=globallike,bg="white"))
             likey[-1].place(x=55+185*j-25,y=240+235*i+1)
             
-            dislikey.append(Label(root,image=globaldislike,bg="#c6e5dc"))
+            dislikey.append(Label(root,image=globaldislike,bg="white"))
             dislikey[-1].place(x=55+185*j+17,y=240+235*i+1)
             
-            sharelist.append(Label(root,image=gshare,bg="#c6e5dc"))
+            sharelist.append(Label(root,image=gshare,bg="white"))
             sharelist[-1].place(x=55+185*j+100,y=240+235*i+1)
             
-            clist.append(Label(root,image=comment,bg="#c6e5dc"))
+            clist.append(Label(root,image=comment,bg="white"))
             clist[-1].place(x=55+185*j+65,y=240+235*i+1)
 
 
 
-            usernameLabels.append(Label(root, text=users[2*i+j].getUsername(), bg="#c6e5dc"))
+            usernameLabels.append(Label(root, text=users[2*i+j].getUsername(), bg="white"))
             usernameLabels[-1].config(font=("Calibri", 15))
             usernameLabels[-1].place(x=55+185*j, y=42+235*i)
 
             profileImages.append(ImageTk.PhotoImage(users[2*i+j].getProfilePic().resize((20,20), Image.ANTIALIAS)))
-            profileLabels.append(Label(root,image=profileImages[-1],bg="#c6e5dc"))
-            profileLabels[-1].place(x=20+185*j, y=40+235*i)
+            profileLabels.append(Label(root,image=profileImages[-1],bg="white"))
+            profileLabels[-1].place(x=20+185*j, y=42+235*i)
 
             postImages.append(ImageTk.PhotoImage(users[2*i+j].getPost().getImage().resize((165,165), Image.ANTIALIAS)))
-            postLabels.append(Label(root, image=postImages[-1]))
+            postLabels.append(Label(root, image=postImages[-1],bg="white"))
             postLabels[-1].place(x=10+185*j, y=70+235*i)
 
-            countLabels.append(Label(root,text=users[2*i+j].getPost().getVoteCount(),bg="#c6e5dc"))
+            countLabels.append(Label(root,text=users[2*i+j].getPost().getVoteCount(),bg="white"))
             countLabels[-1].config(font=("Calibri",12))
             countLabels[-1].place(x=55+185*j,y=240+235*i+1)
     
@@ -85,30 +86,30 @@ def vote(users):
     if (len(users) % 2 != 0):
         i = len(users) // 2
     
-        roundies.append(Label(root,image=tempimage))
-        roundies[-1].place(x=20-gx, y=40+235*i)
+        roundies.append(Label(root,image=tempimage,bg="#c6e5dc"))
+        roundies[-1].place(x=17-gx, y=38+235*i)
         likey.append(Label(root,image=globallike))
         likey[-1].place(x=30,y=240+235*i+1)
-        dislikey.append(Label(root,image=globaldislike,bg="#c6e5dc"))
+        dislikey.append(Label(root,image=globaldislike,bg="white"))
         dislikey[-1].place(x=72,y=240+235*i+1)
-        sharelist.append(Label(root,image=gshare,bg="#c6e5dc"))
+        sharelist.append(Label(root,image=gshare,bg="white"))
         sharelist[-1].place(x=155,y=240+235*i+1)
-        clist.append(Label(root,image=comment,bg="#c6e5dc"))
+        clist.append(Label(root,image=comment,bg="white"))
         clist[-1].place(x=120,y=240+235*i+1)
 
-        usernameLabels.append(Label(root, text=users[-1].getUsername(), bg="#c6e5dc"))
+        usernameLabels.append(Label(root, text=users[-1].getUsername(), bg="white"))
         usernameLabels[-1].config(font=("Calibri", 15))
         usernameLabels[-1].place(x=55, y=42+235*i)
 
         profileImages.append(ImageTk.PhotoImage(users[-1].getProfilePic().resize((20,20), Image.ANTIALIAS)))
-        profileLabels.append(Label(root,image=profileImages[-1]))
-        profileLabels[-1].place(x=20, y=40+235*i)
+        profileLabels.append(Label(root,image=profileImages[-1],bg="white"))
+        profileLabels[-1].place(x=20, y=42+235*i)
 
         postImages.append(ImageTk.PhotoImage(users[-1].getPost().getImage().resize((165,165), Image.ANTIALIAS)))
-        postLabels.append(Label(root, image=postImages[-1]))
+        postLabels.append(Label(root, image=postImages[-1],bg="white"))
         postLabels[-1].place(x=10, y=70+235*i)
 
-        countLabels.append(Label(root,text=users[-1].getPost().getVoteCount(),bg="#c6e5dc"))
+        countLabels.append(Label(root,text=users[-1].getPost().getVoteCount(),bg="white"))
         countLabels[-1].config(font=("Calibri",12))
         countLabels[-1].place(x=55,y=240+235*i+1)
 
